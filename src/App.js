34b5c1644701075d5b71660cc903ffac69691215
Drew Ross <mail.drew.ross@gmail.com>
2020-05-12 12:46:16 -0700

    Map posts with PostsPage using basic data.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import data from './dummy-data';
+import SearchBarContainer from './components/SearchBar/SearchBarContainer';
+import PostsPage from './components/PostsContainer/PostsPage';
 // import the PostsPage and SearchBar and add them to the App
 
 
 const App = () => {
 
+  const posts = data;
+  console.log(posts)
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBarContainer />
+      {
+        posts.map((post, key) => {
+          return <PostsPage key={key} post={post}/>
+        })
+      }
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -5,11 +5,15 @@ import Post from "./Post";
 import "./Posts.css";
 // import data 
 
-const PostsPage = () => {
+const PostsPage = (props) => {
   // set up state for your data
+
+  const { post } = props;
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      
     </div>
   );
 };
